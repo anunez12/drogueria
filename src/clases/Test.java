5d7b2c27df6cc0428a09fc98eec6a2f75be6871a
Albin nunez <albin.ernesto78@hotmail.com>
2016-11-16 12:21:11 -0500

    Detalles Anexados Al Proyecto Parte Cuatro (Nota: Test Herencia drogueria_persona_domicilio)

--- src/clases/Test.java
+++ src/clases/Test.java
@@ -13,14 +13,19 @@ package clases;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
+        
+        System.out.println("clase domicilio");
+      
         Domicilio lachiki = new Domicilio("acetaminofen", "2", "1000", "comercial", "lucero", "calle 48b-34#41");
         System.out.println("El medicamento a comprar es:" + lachiki.getMedicamento());
         System.out.println("La cantidad a comprar es:" + lachiki.getCantidadmedicamento());
         System.out.println("El precio del medicamento a comprar es:" + lachiki.getPrecio());
         System.out.println("El tipo de medicamento a comprar es:" + lachiki.getTipo());
         System.out.println("El barrio a llevar el domicilio es:" + lachiki.getBarrio());
-        System.out.println("La direccion a llevar el medicamento a domicilio es:" + lachiki.getDireccion());
+        System.out.println("La direccion a llevar el medicamento a domicilio es:" + lachiki.getDireccion()); 
+        System.out.println("");
+        System.out.println("clase persona");
 
         Persona p = new Persona("novalgina", " 1", "12500", "generico", "32891918", "Annys", "Perez", "37", "femenino");
         System.out.println("El medicamento a comprar es:" + p.getMedicamento());
@@ -35,3 +40,4 @@ public class Test {
     }
 
 }
+     
