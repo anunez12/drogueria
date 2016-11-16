dde5749121db7af46506108f44cdd27c79c0cd1f
Albin nunez <albin.ernesto78@hotmail.com>
2016-11-16 13:03:26 -0500

    Detalles Anexados Al Proyecto Parte Cuatro (Nota: mejoras)

--- src/interfaz/Administrador.java
+++ src/interfaz/Administrador.java
@@ -7,7 +7,7 @@ package interfaz;
 
 import clases.Helper;
 
-/**           
+/**
  *
  * @author SP
  */
@@ -17,7 +17,7 @@ public class Administrador extends javax.swing.JFrame {
      * Creates new form Administrador
      */
     public Administrador() {
-        initComponents();    
+        initComponents();
     }
 
     /**
@@ -29,13 +29,16 @@ public class Administrador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         txtUsuario = new javax.swing.JTextField();
         txtContraseña = new javax.swing.JPasswordField();
+        cmbSalir = new javax.swing.JComboBox<>();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        cmdDrogueria = new javax.swing.JButton();
         cmbDrogueria = new javax.swing.JComboBox();
+        Salir = new javax.swing.JCheckBox();
+        ingresar = new javax.swing.JCheckBox();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -57,6 +60,9 @@ public class Administrador extends javax.swing.JFrame {
         });
         jPanel1.add(txtContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 90, 90, -1));
 
+        cmbSalir.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Salir" }));
+        jPanel1.add(cmbSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 150, 90, -1));
+
         jLabel2.setFont(new java.awt.Font("Tw Cen MT", 2, 24)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(153, 0, 204));
         jLabel2.setText("Usuario");
@@ -67,16 +73,27 @@ public class Administrador extends javax.swing.JFrame {
         jLabel3.setText("Contraseña");
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 80, 100, -1));
 
-        cmdDrogueria.setText("Drogueria");
-        cmdDrogueria.addActionListener(new java.awt.event.ActionListener() {
+        cmbDrogueria.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Ingresar" }));
+        cmbDrogueria.setEnabled(false);
+        jPanel1.add(cmbDrogueria, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 120, 90, -1));
+
+        buttonGroup1.add(Salir);
+        Salir.setText("Salir");
+        Salir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cmdDrogueriaActionPerformed(evt);
+                SalirActionPerformed(evt);
             }
         });
-        jPanel1.add(cmdDrogueria, new org.netbeans.lib.awtextra.AbsoluteConstraints(379, 130, 90, -1));
+        jPanel1.add(Salir, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 210, 70, -1));
 
-        cmbDrogueria.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Ingresar ", "Salir" }));
-        jPanel1.add(cmbDrogueria, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 160, 90, -1));
+        buttonGroup1.add(ingresar);
+        ingresar.setText("Ingresar");
+        ingresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ingresarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(ingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 180, 70, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/administrador.jpg"))); // NOI18N
         jLabel1.setText("jLabel1");
@@ -97,7 +114,29 @@ public class Administrador extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void cmdDrogueriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdDrogueriaActionPerformed
+    private void txtUsuarioKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsuarioKeyTyped
+        char c = evt.getKeyChar();
+        if (Character.isSpaceChar(c)) {
+            getToolkit();
+            evt.consume();
+        }
+    }//GEN-LAST:event_txtUsuarioKeyTyped
+
+    private void txtContraseñaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtContraseñaKeyTyped
+        char c = evt.getKeyChar();
+        if (Character.isSpace(c)) {
+            getToolkit();
+            evt.consume();                                   
+        }
+    }//GEN-LAST:event_txtContraseñaKeyTyped
+
+    private void ingresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresarActionPerformed
+       /* if (ingresar.isSelected()) {
+       
+        }else if(ingresar.isSelected()==false){ 
+           cmdDrogueria.setEnabled(false);
+        } 
+     */ 
         String usuario = "virgilio";
         String contraseña = "administrador";
         int op = cmbDrogueria.getSelectedIndex();
@@ -113,27 +152,26 @@ public class Administrador extends javax.swing.JFrame {
                     Principal.setVisible(true);
                 }
                 break;
-            case 1:
-                System.exit(0);
-                break;  
+           
+        } 
+        if(ingresar.isSelected()){ 
+          cmbDrogueria.setEnabled(true);
+        }else if(ingresar.isSelected()==false){ 
+          cmbDrogueria.setEnabled(false);
         }
-    }//GEN-LAST:event_cmdDrogueriaActionPerformed
+    }//GEN-LAST:event_ingresarActionPerformed
 
-    private void txtUsuarioKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsuarioKeyTyped
-        char c = evt.getKeyChar();
-        if (Character.isSpaceChar(c)) {
-            getToolkit();
-            evt.consume();
-        }
-    }//GEN-LAST:event_txtUsuarioKeyTyped
-
-    private void txtContraseñaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtContraseñaKeyTyped
-       char c = evt.getKeyChar();
-        if (Character.isSpace(c)) {
-            getToolkit();
-            evt.consume();
-        }  
-    }//GEN-LAST:event_txtContraseñaKeyTyped
+    private void SalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_SalirActionPerformed
+       int op = cmbDrogueria.getSelectedIndex(); 
+       
+       switch (op){ 
+           case 0: 
+         System.exit(0); 
+         break;   
+       } 
+        
+        
+    }//GEN-LAST:event_SalirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -171,8 +209,11 @@ public class Administrador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox Salir;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JComboBox cmbDrogueria;
-    private javax.swing.JButton cmdDrogueria;
+    private javax.swing.JComboBox<String> cmbSalir;
+    private javax.swing.JCheckBox ingresar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
